1f09fe8c8c04a9cd32cca42abdd9f3cec862128b
jjade seravla <jadealvares@hotmail.co.uk>
2017-12-22 13:03:18 +0000

    added items via node for get request

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,8 @@
+var data = [{item: 'buy an xmas present'}, {item: 'let Olly out'}, {item: 'go to sleep early'}];
+
 module.exports = function(app) {
   app.get('/todo', function(req, res) {
-    res.render('todo');
+    res.render('todo', {todos: data});
   });
 
   app.post('/todo', function(req, res) {
