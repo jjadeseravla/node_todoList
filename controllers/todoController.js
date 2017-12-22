629e71cef3041afa7d23ea379a5222cc22d513aa
jjade seravla <jadealvares@hotmail.co.uk>
2017-12-22 14:13:36 +0000

    post request to add item and render it on page

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,12 +1,16 @@
+var bodyParser = require('body-parser');
+
 var data = [{item: 'buy an xmas present'}, {item: 'let Olly out'}, {item: 'go to sleep early'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app) {
   app.get('/todo', function(req, res) {
     res.render('todo', {todos: data});
   });
 
-  app.post('/todo', function(req, res) {
-
+  app.post('/todo', urlencodedParser, function(req, res) {
+    data.push(req.body);
+    res.json(data);
   });
 
   app.delete('/todo', function(req, res) {
