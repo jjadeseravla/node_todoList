200f13317183fb3129a117b5a9c8f69671a3ae12
jjade seravla <jadealvares@hotmail.co.uk>
2017-12-22 14:34:08 +0000

    connected to mongo db

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,19 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect('mongodb://test:test@ds137530.mlab.com:37530/tod0jjadeseravla');
+
+//create a schema - like  a blue print for our data
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'buy healthy food'}).save(function(err) {
+  if (err) throw err;
+  console.log('item.saved');
+});
 
 var data = [{item: 'buy an xmas present'}, {item: 'let Olly out'}, {item: 'go to sleep early'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
